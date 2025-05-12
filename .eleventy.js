9b6e14801590947fbec226164ac4fbe3b57a963f
kalpih <kalle.pihlainen@utu.fi>
2025-05-12 09:41:57 -0400

    Path correction

--- .eleventy.js
+++ .eleventy.js
@@ -1,26 +1,26 @@
 // .eleventy.js
 module.exports = function(eleventyConfig) {
   // Tell Eleventy to copy the 'css' directory to the output folder.
-  // We'll move your style.css into a 'css' folder shortly.
-  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("css"); // Assumes 'css' folder is in your project root
 
   // Tell Eleventy to copy the 'js' directory to the output folder.
-  // We'll move your script.js into a 'js' folder shortly.
-  eleventyConfig.addPassthroughCopy("js");
+  eleventyConfig.addPassthroughCopy("js");   // Assumes 'js' folder is in your project root
 
   // Tell Eleventy to copy your 'images' directory if you have one.
-  // Make sure it's in the root of your project or adjust the path.
-  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("images"); // Assumes 'images' folder is in your project root
 
   // You can return your Config object (optional, but good practice).
   return {
     // When a passthrough file is modified, rebuild the site appropriately.
     passthroughFileCopy: true,
     dir: {
-      input: ".",          // Root folder for source files (current directory).
+      input: ".",         // Root folder for source files (current directory).
       includes: "_includes", // Folder for reusable HTML partials.
       layouts: "_layouts",   // Folder for HTML page layouts.
       output: "docs"      // Folder where the generated static site will go.
-    }
+    },
+    // === ADD OR ENSURE THIS LINE IS CORRECT ===
+    pathPrefix: "/kallepihlainen/" // <<< THIS IS THE KEY!
+    // ========================================
   };
 };
